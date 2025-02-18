26c3edf3200925412deee5c43f0eeca53fede6cf
David Iyera <davidiyera@Davids-MacBook-Pro.local>
2025-02-18 22:35:02 +0200

    applied separation of concern on routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,6 @@
-import {useRef} from "react";
-import Home from "./sections/Home";
-import Hero from "./sections/Hero";
-import Services from "./sections/Services";
-import Industries from "./sections/Industries";
-import Innovation from "./sections/Innovation";
-import About from "./sections/About";
-import Main from "./sections/Main";
-import { Contact } from "./sections/Contact";
-import Footer from "./sections/Footer";
-import Scroll from "./sections/Scroll";
-import PrivacyPolicy from "./sections/privacyPolicy";
+import Index from "./sections/Index";
+import PrivacyPolicy from "./sections/PrivacyPolicy";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-
-
-const Index = () => {
-  const industrySectionRef = useRef(null);
-  const servicesSectionRef = useRef(null);
-  const innovationSectionRef = useRef(null);
-
-  return (
-    <>
-      <Home  innovationSectionRef={innovationSectionRef} />
-      <Hero industrySectionRef={industrySectionRef} servicesSectionRef={servicesSectionRef} />
-      <Services servicesSectionRef={servicesSectionRef} />
-      <Industries industrySectionRef={industrySectionRef} />
-      <Innovation innovationSectionRef={innovationSectionRef} />
-      <About />
-      <Main />
-      <Contact />
-      <Footer />
-      <Scroll />
-    </>
-  )
-}
 
 
 const App = () => {
---
+++ src/sections/Index.jsx
@@ -0,0 +1,34 @@
+import {useRef} from "react";
+import Home from "./Home";
+import Hero from "./Hero";
+import Services from "./Services";
+import Industries from "./Industries";
+import Innovation from "./Innovation";
+import About from "./About";
+import Main from "./Main";
+import { Contact } from "./Contact";
+import Footer from "./Footer";
+import Scroll from "./Scroll";
+
+const Index = () => {
+    const industrySectionRef = useRef(null);
+    const servicesSectionRef = useRef(null);
+    const innovationSectionRef = useRef(null);
+  
+    return (
+      <>
+        <Home  innovationSectionRef={innovationSectionRef} />
+        <Hero industrySectionRef={industrySectionRef} servicesSectionRef={servicesSectionRef} />
+        <Services servicesSectionRef={servicesSectionRef} />
+        <Industries industrySectionRef={industrySectionRef} />
+        <Innovation innovationSectionRef={innovationSectionRef} />
+        <About />
+        <Main />
+        <Contact />
+        <Footer />
+        <Scroll />
+      </>
+    )
+}
+
+export default Index;
