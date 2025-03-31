d8954e50613508411026618bd83dc857e5b350b0
bahatisteven <bahatisteven92@gmail.com>
2025-04-01 01:19:04 +0200

    added terms and services page

---
+++ src/sections/TermsAndServices.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const TermsAndServices = () => {
+  return (
+        <iframe       
+            src="https://www.termsfeed.com/live/59277cfc-57cf-4cc9-a934-6246705d7443"
+            width="100%"
+            height="1024px"
+            style={{ border: "none" }}
+            title="External Page"
+        ></iframe>
+  )
+}
+
+export default TermsAndServices;
